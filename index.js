1dc9895ea98ddfb70bd98902a3ff7e8b298920bb
Travis Puryear <travispuryear678@gmail.com>
2021-09-08 17:45:19 -0400

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+var vid = document.getElementById("video");
+vid.playbackRate = 1;
+
+
+const menuToggle = document.querySelector('.toggle')
+const showcase = document.querySelector('.showcase')
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active')
+    showcase.classList.toggle('active')
+})
